828f0911fa5777bfc14c23a47514c90fc68ac577
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2023-09-08 01:25:59 -0300

    criacao do projeto jarbasGPT, ajuste da chave KEY

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const formPerguntaChat = document.getElementById('form-pergunta-chat');
 
 // Chave da API do OPENAI
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "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";
 
 
 if (formPerguntaChat) {
